8ff2b23ee76f8795886bffcfe6ed4c6cb351054f
tpahare <tpahare@andrew.cmu.edu>
2016-02-13 12:25:57 -0500

    Modified employee bean as per the current specification

--- src/com/databean/EmployeeBean.java
+++ src/com/databean/EmployeeBean.java
@@ -3,18 +3,39 @@ package com.databean;
 import org.genericdao.PrimaryKey;
 
 /**
- * @author Xuesong Zhang (Andrew ID: xuesongz)
+ * @author tpahare
  */
 
 @PrimaryKey("username")
 public class EmployeeBean {
-	private String username;
 	private String firstname;
 	private String lastname;
+	private String username;
 	private String password;
-	
+	private String address1;
+	private String address2;
+	private String city;
+	private String state;
+	private String zip;
 	public EmployeeBean() {
 		
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	
+	
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
 	}
 
 	public String getUsername() {
@@ -25,28 +46,52 @@ public class EmployeeBean {
 		this.username = username;
 	}
 
-	public String getFirstname() {
-		return firstname;
-	}
-
-	public void setFirstname(String firstname) {
-		this.firstname = firstname;
-	}
-
-	public String getLastname() {
-		return lastname;
-	}
-
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
-	}
-
 	public String getPassword() {
 		return password;
 	}
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public String getAddress1() {
+		return address1;
+	}
+
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+
+	public String getAddress2() {
+		return address2;
+	}
+
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
 	}
 
 
