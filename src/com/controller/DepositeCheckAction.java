cab17d641534df89d2935162dc33c8f280c998e6
xuanqu <xuanqu@xuans-air.wv.cc.cmu.edu>
2016-02-13 20:12:23 -0500

    aa

--- src/com/controller/DepositeCheckAction.java
+++ src/com/controller/DepositeCheckAction.java
@@ -15,14 +15,19 @@ import com.databean.CustomerBean;
 import com.databean.EmployeeBean;
 import com.databean.TransactionBean;
 import com.form.DepositeCheckForm;
+import com.google.gson.Gson;
 import com.model.CustomerDAO;
 import com.model.Model;
 import com.model.TrancDAO;
+import com.view.Menu;
+import com.view.Message;
 
 public class DepositeCheckAction extends Action {
 	private FormBeanFactory<DepositeCheckForm> formBeanFactory = FormBeanFactory.getInstance(DepositeCheckForm.class);
 	CustomerDAO customerDAO;
 	TrancDAO trancDAO;
+	Gson gson = new Gson();
+	Message message = new Message();
 	public DepositeCheckAction(Model model) {
 		this.customerDAO = model.getCustomerDAO();
 		this.trancDAO = model.getTrancDAO();
@@ -43,15 +48,21 @@ public class DepositeCheckAction extends Action {
 		EmployeeBean employee = (EmployeeBean) session.getAttribute("employee");
 		
 		if(employee == null) {
-			errors.add("Please Login first");
-			return "EmployeeLogin.do";
+			errors.add("You must log in prior to making this request");
+			message.setMessage(errors.toString());
+			gson.toJson(message);
+			//return "EmployeeLogin.do";
+			return gson.toJson(message);
 		}
-		
+
 		try{
-			String customer = request.getParameter("depositcheckcid");
-			if (customer == null || customer.length() == 0) {
-				return "ViewCustomerAccount.do";
-			}
+//			String customer = request.getParameter("username");
+//			if (customer == null || customer.length() == 0) {
+//				errors.add("I’m sorry, there was a problem depositing the money");
+//				message.setMessage(errors.toString());
+//				return gson.toJson(message);
+				//return "ViewCustomerAccount.do";
+//			}
 //			if (request.getParameter("button") == null) {
 //				return "ViewCustomerAccount.do";
 //			}
@@ -62,22 +73,31 @@ public class DepositeCheckAction extends Action {
 			DepositeCheckForm form = formBeanFactory.create(request);
 			request.setAttribute("form",form);
 			
-			int depositcheckcid = Integer.parseInt(request.getParameter("depositcheckcid"));
-	        request.setAttribute("depositcheckcid", depositcheckcid);
-			
-			if(!form.isPresent()){
-				return "DepositCheck.jsp";
+			String username = form.getUsername();
+			if (username == null || username.length() == 0) {
+				errors.add("I’m sorry, there was a problem depositing the money");
+				message.setMessage(errors.toString());
+				return gson.toJson(message);
 			}
+	        request.setAttribute("username", username);
+	        CustomerBean[] customerBean = customerDAO.match(MatchArg.equals("username", username));
+	        int id = customerBean[0].getCid();
+			
+//			if(!form.isPresent()){
+//				return "DepositCheck.jsp";
+//			}
 			
 //			System.out.print("Username here:" + form.getUsername());
 //			System.out.print("Amount here:" + form.getAmount());
-			System.out.print("Deposit Check look 1");
+//			System.out.print("Deposit Check look 1");
 			
 			
 			errors.addAll(form.getValidationErrors());
 			
 			if(errors.size()!=0){
-				return "DepositCheck.jsp";
+				message.setMessage(errors.toString());
+				return gson.toJson(message);
+				//return "DepositCheck.jsp";
 			}
 			
 			
@@ -91,24 +111,24 @@ public class DepositeCheckAction extends Action {
 			//System.out.println("hh");
 			
 			TransactionBean tBean = new TransactionBean();
-			tBean.setCid(depositcheckcid);
+			tBean.setCid(id);
 			tBean.setTransactiontype("deposit");
 			
-			long depositmoney = (long) (100 * Double.parseDouble(form.getAmount()));
+			long depositmoney = (long) (100 * Double.parseDouble(form.getCash()));
 			
 			tBean.setAmount(depositmoney);
 			
 			trancDAO.create(tBean);
 			
-			System.out.print("look 5");
-			
-			return "DepositCheckSuccess.jsp";
+			//System.out.print("look 5");
+			message.setMessage("The account has been successfully updated");
+			return gson.toJson(message);
 		}catch (RollbackException e) {
-        	errors.add(e.getMessage());
-        	return "error.jsp";
+			message.setMessage(e.getMessage());
+			return gson.toJson(message);
         } catch (FormBeanException e) {
-        	errors.add(e.getMessage());
-        	return "error.jsp";
+        	message.setMessage(e.getMessage());
+			return gson.toJson(message);
         }
 		
 		
--- src/com/form/DepositeCheckForm.java
+++ src/com/form/DepositeCheckForm.java
@@ -6,24 +6,24 @@ import java.util.List;
 import org.mybeans.form.FormBean;
 
 public class DepositeCheckForm extends FormBean {
-//	private String username;
-	private String amount;
+	private String username;
+	private String cash;
 	private String action;
 
-//	public String getUsername() {
-//		return username;
-//	}
-//
-//	public void setUsername(String username) {
-//		this.username = username;
-//	}
-
-	public String getAmount() {
-		return amount;
+	public String getUsername() {
+		return username;
 	}
 
-	public void setAmount(String amount) {
-		this.amount = amount;
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getCash() {
+		return cash;
+	}
+
+	public void setCash(String cash) {
+		this.cash = cash;
 	}
 
 	public String getAction() {
@@ -40,22 +40,18 @@ public class DepositeCheckForm extends FormBean {
 
 	public List<String> getValidationErrors() {
 		List<String> errors = new ArrayList<String>();
-//		if (username == null || username.trim().length() == 0) {
-//			errors.add("Username not present");
-//		}
-//		if (username.matches(".*[<>\"].*")) {
-//			errors.add("Username may not contain angle brackets or quotes");
-//		}
+		if (username == null || username.trim().length() == 0) {
+			errors.add("Username not present");
+		}
+		if (username.matches(".*[<>\"].*")) {
+			errors.add("Username may not contain angle brackets or quotes");
+		}
 		
-		System.out.print("Step1:" + amount);
-		
-		if (amount == null || amount.trim().length() == 0) {
+		if (cash == null || cash.trim().length() == 0) {
 			errors.add("Amount is required");
 		}
 		
-		System.out.print("Step2:" + amount);
-		
-		if (amount.matches(".*[<>\"].*")) {
+		if (cash.matches(".*[<>\"].*")) {
 			errors.add("Amount may not contain angle brackets or quotes");
 		}
 		
@@ -63,15 +59,15 @@ public class DepositeCheckForm extends FormBean {
 			return errors;
 		}
 		try {
-			if(amount.matches(".*\\s+.*")) {
+			if(cash.matches(".*\\s+.*")) {
 				errors.add("Amount should be a number, no white spaces");
 				return errors;
 			}
-			if (Float.parseFloat(amount) <= 0) {
+			if (Float.parseFloat(cash) <= 0) {
 				errors.add("Amount can not be negetive");
 				return errors;
 			}
-			if (Float.parseFloat(amount) > 1000000) {
+			if (Float.parseFloat(cash) > 1000000) {
 				errors.add("Amount should be Less than one million");
 				return errors;
 			}
